de02e719d872fe804711974ee88e3716cac8c9f1
Lavanya Narang <159886692+Lavanya-nrg@users.noreply.github.com>
2024-02-26 12:17:39 +0530

    Update panda_queries.py

--- asssignment_solutions/Milestone3/panda_queries.py
+++ asssignment_solutions/Milestone3/panda_queries.py
@@ -14,13 +14,13 @@ engine=create_engine('mysql+pymysql://'+username+':'+encoded_password+'@'+hostna
 conn=engine.connect()
 
 def AvgSalaryOfEmployee():
-    sql_query1=pd.read_sql_query("SELECT AVG(salary) FROM employee",conn)
+    sql_query1=pd.read_sql_query("SELECT ROUND(AVG(E.SALARY)) AS AVGSAL FROM Employee_Table;",conn)
     df=pd.DataFrame(sql_query1)
     print(df)
 
 
 def AvgSalaryDeptWise():
-    sql_query2=pd.read_sql_query("SELECT d.deptName as Department , AVG(e.salary) as avg_salary FROM department d JOIN employee e ON d.id=e.deptId GROUP BY d.deptName",conn)
+    sql_query2=pd.read_sql_query("SELECT E.DEPT_ID,D.DEPT_NAME,ROUND(AVG(E.SALARY),2) AS AVGSAL FROM Employee_Table AS E INNER JOIN Department_Table AS D ON E.DEPT_ID=D.DEPT_ID GROUP BY D.DEPT_ID ORDER BY E.DEPT_ID ASC;",conn)
     df=pd.DataFrame(sql_query2)
     print(df)
 
@@ -30,17 +30,17 @@ def DeptSpendMaxSalary():
     print(df)
 
 def DeptMaleDominated():
-    sql_query4=pd.read_sql_query("SELECT d.deptName AS department, COUNT(CASE WHEN e.gender = 'Male' THEN 1 END) AS male_count FROM department d JOIN employee e ON d.id = e.deptId GROUP BY d.deptName ORDER BY male_count DESC LIMIT 1",conn)
+    sql_query4=pd.read_sql_query("SELECT E.DEPT_ID,D.DEPT_NAME,SUM(E.SLARY) AS DEPTSAL FROM Employee_Table AS E INNER JOIN Department_Table AS D ON D.DEPT_ID=E.DEPT_ID GROUP BY E.DEPT_ID  ORDER BY DEPTSAL LIMIT 1;",conn)
     df=pd.DataFrame(sql_query4)
     print(df)
 
 def MonthMaxIndianCelebrateBirthday():
-    sql_query5=pd.read_sql_query("SELECT DATEPART(MONTH, birthdate) AS birth_month, COUNT(*) AS emp_count FROM employee WHERE nationality = 'Indian' GROUP BY DATEPART(MONTH, birthdate) ORDER BY emp_count DESC LIMIT 1",conn)
+    sql_query5=pd.read_sql_query("SELECT EXTRACT(MONTH FROM DATE_OF_BIRTH) AS BMONTH FROM Employee_Table WHERE NATIONALITY='INDIAN' GROUP BY BMONTH ORDER BY BMONTH DESC LIMIT 1;",conn)
     df=pd.DataFrame(sql_query5)
     print(df)
 
 def AvgSalaryDeptHeads():
-    sql_query6=pd.read_sql_query("SELECT AVG(salary) AS avg_salary_department_heads FROM employee e where e.isHead='Yes'",conn)
+    sql_query6=pd.read_sql_query("SELECT D.DEPT_HEAD_ID,ROUND(AVG(E.SALARY),2) AS AVGHEADSAL FROM E_T AS E INNER JOIN Department_Table AS D GROUP BY D.DEPT_HEAD_ID ORDER BY D.DEPT_HEAD_ID ASC;",conn)
     df=pd.DataFrame(sql_query6)
     print(df)
 
